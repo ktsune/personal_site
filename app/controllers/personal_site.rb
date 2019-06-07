73b7db7da8609392af78db88de159e173bd17f24
Sarah Tatro <statrox4@gmail.com>
2019-06-07 17:23:32 -0600

    Add views and testing

---
+++ app/controllers/personal_site.rb
@@ -0,0 +1,30 @@
+require 'rack'
+
+equire 'rack'
+
+class PersonalSite
+  def self.call(env)
+    case env["PATH_INFO"]
+    when '/' then index
+    when '/about' then about
+    else
+      error
+    end
+  end
+
+  def self.index
+    render_view('index.html')
+  end
+
+  def self.about
+    render_view('about.html')
+  end
+
+  def self.error
+    render_view('error.html', '404')
+  end
+
+  def self.render_view(page, code = '200')
+    [code, {'Content-Type' => 'text/html'}, [File.read("./app/views/#{page}")]]
+  end
+end
